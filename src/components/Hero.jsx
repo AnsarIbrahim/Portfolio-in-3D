2095099521c35f27728eb4d1e930c9e8929a6739
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-08-11 21:16:17 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
-// import { ComputersCanvas } from './canvas';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
